a4430402460bcefc5161e53d4456e817659ea25b
mk-404lab <alsgur9984@naver.com>
2026-05-07 09:51:19 +0900

    feat: 14-1강 - findByUsernameAndEitherPasswordOrNickname 네이밍 메서드 생성 안됨에 따라 직접 쿼리 작성

--- backend/src/main/kotlin/com/back/domain/member/repository/MemberRepository.kt
+++ backend/src/main/kotlin/com/back/domain/member/repository/MemberRepository.kt
@@ -2,6 +2,7 @@ package com.back.domain.member.repository
 
 import com.back.domain.member.entity.Member
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface MemberRepository : JpaRepository<Member, Int>, MemberRepositoryCustom {
 
@@ -9,4 +10,7 @@ interface MemberRepository : JpaRepository<Member, Int>, MemberRepositoryCustom 
     fun findByApiKey(apiKey: String): Member?
     fun findByIdIn(ids: List<Int>): List<Member>
     fun findByUsernameAndNickname(username: String, nickname: String): Member?
+
+    @Query("SELECT m FROM Member m WHERE m.username = :username AND (m.password = :password OR m.nickname = :nickname)")
+    fun findCByUsernameAndEitherPasswordOrNickname(username: String, password: String, nickname: String): List<Member>
 }
--- backend/src/test/kotlin/com/back/domain/member/repository/MemberRepositoryTest.kt
+++ backend/src/test/kotlin/com/back/domain/member/repository/MemberRepositoryTest.kt
@@ -75,4 +75,16 @@ class MemberRepositoryTest {
         assertThat(memberList.map {it.username}).containsAnyOf("user1", "user2")
     }
 
+    @Test
+    fun `findCByUsernameAndEitherPasswordOrNickname()`() {
+        // select * from member where username = ? and (password = ? or nickname = ?)
+
+        val members = memberRepository.findCByUsernameAndEitherPasswordOrNickname("admin", "wrong-password", "운영자")
+
+        assertThat(members).isNotEmpty
+        assertThat(members.any {it.username == "admin" && (it.password == "wrong-password" || it.nickname == "운영자")}).isTrue
+
+
+    }
 }
+
