4417cec885ad2b747b850cb02a09c944f58f11b4
mk-404lab <alsgur9984@naver.com>
2026-05-09 17:09:04 +0900

    feat: 32강 - MemberSearchKeywordType을  enum 타입으로 구현

--- backend/src/main/kotlin/com/back/domain/member/repository/MemberRepositoryCustom.kt
+++ backend/src/main/kotlin/com/back/domain/member/repository/MemberRepositoryCustom.kt
@@ -1,6 +1,7 @@
 package com.back.domain.member.repository
 
 import com.back.domain.member.entity.Member
+import com.back.standard.enums.MemberSearchKeywordType
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
@@ -25,6 +26,6 @@ interface MemberRepositoryCustom {
     fun findQByNicknameContaining(nickname: String, pageable: Pageable): Page<Member>
     fun findQByNicknameContainingOrderByIdDesc(nickname: String): List<Member>
     fun findQByUsernameContaining(username: String, pageable: Pageable): Page<Member>
-    fun findByKwPaged(kw: String,kwType: String, pageable: Pageable): Page<Member>
+    fun findByKwPaged(kw: String, kwType: MemberSearchKeywordType, pageable: Pageable): Page<Member>
 
 }
