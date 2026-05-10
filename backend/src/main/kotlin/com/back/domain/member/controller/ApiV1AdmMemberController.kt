33cd4ebbc3058f4e331e761fff742ea751230100
mk-404lab <alsgur9984@naver.com>
2026-05-10 15:05:53 +0900

    feat: 33~34강 - 정렬 기준을 enum 타입 MemberSearchSortType에 정의 / sort 파라미터로 받아서 정렬 구현

--- backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
+++ backend/src/main/kotlin/com/back/domain/member/controller/ApiV1AdmMemberController.kt
@@ -4,6 +4,7 @@ import com.back.domain.member.dto.MemberWithUsernameDto
 import com.back.domain.member.service.MemberService
 import com.back.standard.dto.PageDto
 import com.back.standard.enums.MemberSearchKeywordType
+import com.back.standard.enums.MemberSearchSortType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -20,13 +21,14 @@ class ApiV1AdmMemberController(
         @RequestParam("page", defaultValue = "1") page: Int,
         @RequestParam("pageSize", defaultValue = "2") pageSize: Int,
         @RequestParam("kw", defaultValue = "") kw: String,
-        @RequestParam("kwType", defaultValue = "ALL") kwType: MemberSearchKeywordType
+        @RequestParam("kwType", defaultValue = "ALL") kwType: MemberSearchKeywordType,
+        @RequestParam("sort", defaultValue = "ID") sort: MemberSearchSortType
     ): PageDto<MemberWithUsernameDto> {
 
         val page = if(page >= 1) page else 1
         val pageSize = if(pageSize >= 5) pageSize else 5
 
-        val pageResult = memberService.findByPaged(page, pageSize, kw, kwType)
+        val pageResult = memberService.findByPaged(page, pageSize, kw, kwType, sort)
 
         return PageDto(
             pageResult.map {
